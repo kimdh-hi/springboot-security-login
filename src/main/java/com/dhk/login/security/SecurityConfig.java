8124fc4042f5476903a3752759889a9418d6f0e0
kimdaehyun <zbeld123@gmail.com>
2021-08-01 17:11:36 +0900

    warn ..

--- src/main/java/com/dhk/login/security/SecurityConfig.java
+++ src/main/java/com/dhk/login/security/SecurityConfig.java
@@ -1,18 +1,21 @@
 package com.dhk.login.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final KakaoOauth2UserService kakaoOAuth2UserService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        http.authorizeRequests().antMatchers("/","/registry","/login", "/css/**").permitAll()
-                .anyRequest().authenticated();
+        http.oauth2Login().userInfoEndpoint().userService(kakaoOAuth2UserService);
+        http.logout().logoutSuccessUrl("/");
     }
 }
